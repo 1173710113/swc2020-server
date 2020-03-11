b259cec02e2684a922a63ac4b9e46ebce3a942be
Azc <501366309@qq.com>
2020-03-11 22:03:13 +0800

    完成date抽取

--- sw2020_server/src/main/java/com/example/demo/utils/DateRecognize.java
+++ sw2020_server/src/main/java/com/example/demo/utils/DateRecognize.java
@@ -1,13 +1,22 @@
 package com.example.demo.utils;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import org.python.antlr.PythonParser.return_stmt_return;
+import com.alibaba.fastjson.JSONObject;
+import com.example.demo.domain.TimeExtractResult;
 import com.example.demo.utils.SyntaxAnalyzer;
+import com.example.demo.utils.configReader.Configreader;
+import com.example.demo.utils.inputstrategy.InputStrategy;
 
 public class DateRecognize {
   
@@ -75,12 +84,13 @@ public class DateRecognize {
 //    lessVOBSet.retainAll(vOBSet);
     
     String results;
-    if (analyzeList.get(0).size() > 0) {
+    if (analyzeList.get(0).size() > 0) {//vob类型含有关键词
       for (String vob: analyzeList.get(0)) {
         results = syntaxAnalyzer.fatherMap.get(vob) + " " + vob + " ";
         return results;
       }
     }
+    //SBV类型含有关键词
     if (analyzeList.get(3).size() > 0 && analyzeList.get(2).size() > 0) {
       for (String sbv: analyzeList.get(3)) {
         for (String coo: analyzeList.get(2)) {
@@ -100,18 +110,41 @@ public class DateRecognize {
     return null;
   }
   
-  public static void main(String[] args) throws FileNotFoundException {
-    DateRecognize dateRecognize = new DateRecognize();
-    BufferedReader testReader = new BufferedReader(new FileReader("./syntaxTest.txt"));
-    String testString;
-    try {
-      while ((testString = testReader.readLine()) != null) {
-        System.out.println(testString);
-        System.out.println(dateRecognize.recognize(testString));
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
+  public static void main(String[] args) throws NumberFormatException, UnknownHostException, IOException, ParseException {
+    Configreader config = Configreader.reader("config.xml");
+    CorrespondUtil corres = new CorrespondUtil(InetAddress.getByName(config.readItem("DateExtractorClient")),
+            InetAddress.getByName(config.readItem("DateExtractorServer")),
+            Integer.parseInt(config.readItem("DateExtractorServerPort")),
+            Integer.parseInt(config.readItem("DateExtractorClientPort")));
+    InputStrategy inputStrategy = InputStrategy.input(new File("../syntaxTest.txt"));
+    List<String> list = new ArrayList<String>();
+    String tmp;
+    while ((tmp = inputStrategy.nextLine()) != null) {
+        list.add(tmp);
     }
+    JSONObject obj = corres.sendJson(list);
+    
+    TimeExtractResult ter = new TimeExtractResult(obj);
+    DateRecognize dateRecognize = new DateRecognize();
+    
+    for (int i = 0; i < ter.getNumOfAnalyze(); i++) {
+        System.out.println("sentence = " + ter.getAnalyze(i));
+        System.out.println("Date = " + ter.getDate(i));
+        System.out.println(dateRecognize.recognize(ter.getAnalyze(i)));
+    }
+    
+    
+    
+//    BufferedReader testReader = new BufferedReader(new FileReader("./syntaxTest.txt"));
+//    String testString;
+//    try {
+//      while ((testString = testReader.readLine()) != null) {
+//        System.out.println(testString);
+//        System.out.println(dateRecognize.recognize(testString));
+//      }
+//    } catch (IOException e) {
+//      e.printStackTrace();
+//    }
     
 //    System.out.println(dateRecognize.recognize("1999年，世界上最强的钟哥诞生了。世界从此陷入了钟哥的通知。"));
   }
